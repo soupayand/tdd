9ccd6b7334f394a8383b63fab491efd814428027
soupayand <soupayand@gmail.com>
2021-01-10 20:25:52 +0530

    Added support for "\n" and "," delimited number string

--- calculator/src/main/java/com/tdd/calculator/Calculator.java
+++ calculator/src/main/java/com/tdd/calculator/Calculator.java
@@ -1,13 +1,11 @@
 package com.tdd.calculator;
 
-import org.apache.commons.lang3.StringUtils;
-
 public class Calculator {
 
 	public int add(String input) {
 		int sum = 0;
 		if (!input.isEmpty()) {
-			String[] numbers = StringUtils.split(input, ",");
+			String[] numbers = input.split("[\n,]");
 			for (String number : numbers) {
 				sum = sum + Integer.parseInt(number);
 			}
